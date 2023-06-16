a6c2de1d8258c9ad90cc2a31f9902732be7cc15d
kiana-jahanshid <tannaz.maleki82@gmail.com>
2023-06-16 07:48:15 +0330

    1st

---
+++ Assignment_37_ML/crop_digits/Crop_digits.py
@@ -0,0 +1,24 @@
+import os
+import cv2
+import numpy as np
+ 
+    
+for i in range(10):
+    directory = f"{i}"
+    parent_dir = "mnist"
+    path = os.path.join(parent_dir, directory) 
+    os.makedirs(path)
+
+
+image = cv2.imread("mnist.webp")
+y=0
+for k in range(10):
+
+    for i in range(5):
+        x=0
+        for j in range(100):
+            croped= image[0+y:20+y , 0+x:20+x]
+            x+= 20
+            img = cv2.imwrite(f"mnist/{k}/{k}{i}{j}.jpg" , croped)
+        y += 20
+
